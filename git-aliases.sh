e9b07c6647260af93a7264e6d878c33f78a57001
Julien Lopinto <julien.lopinto@gmail.com>
2024-12-16 14:19:50 +0100

    refined commands and outputs

--- git-aliases.sh
+++ git-aliases.sh
@@ -12,14 +12,15 @@ confirm() {
 }
 
 alias gs='git status'
-alias gbl="git for-each-ref --sort=-committerdate refs/heads --format='%(authordate:short) %(color:red)%(objectname:short) %(color:yellow)%(refname:short)%(color:reset)%(if)%(upstream)%(then) %(upstream:lstrip=3) %(upstream:track)%(end) - %(color:green)%(committerdate:relative)%(color:reset)'"
-alias gb="git branch -vv --sort=-committerdate"
+alias gb="git for-each-ref --sort=-committerdate refs/heads --format='%(authordate:short) %(color:red)%(objectname:short) %(color:yellow)%(refname:short)%(color:reset)%(if)%(upstream)%(then) %(upstream:lstrip=3) %(upstream:track)%(end) - %(color:green)%(committerdate:relative)%(color:reset)'"
 
 alias gpr='git pull --rebase'
 alias gprom='git pull --rebase origin main'
 alias gpf='git push --force-with-lease'
+alias gpfn='git push --force-with-lease --no-verify'
 
 alias gc="git commit"
+alias gcn="git commit --no-verify"
 alias gca="git commit --amend"
 alias gco='git checkout'
 alias gcop='gco -p'
@@ -29,6 +30,7 @@ alias gstp='git stash pop'
 alias gstl='git stash list'
 
 alias gru='git remote update'
+alias grus='git remote update && git status'
 
 alias grh='git reset HEAD'
 alias grH='git reset --hard HEAD'
@@ -38,12 +40,12 @@ alias gap='git add -p'
 alias ga='git add'
 alias gaa='git add -A'
 alias gau='git add -u'
+
 # "git quick push", useful to amend previous commit with small changes
-alias gqp='gau && gc --amend -C HEAD && gpf'
+alias gqp='gau && gc --amend -C HEAD --no-verify && gpf --no-verify'
 alias gwip='gaa && gc -m "WIP" --no-verify' 
-alias gl="git log --graph --all --decorate --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(cyan)<%an>%Creset'"
-alias glme="git log --author='$(git config --get user.name)' --graph --all --decorate --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(cyan)<%an>%Creset'"
-alias glog='git log --graph --oneline --all --decorate'
+alias gl="git log --graph --pretty=format:'%C(cyan)%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(magenta)<%an>%Creset' --abbrev-commit --date=relative"
+alias glme="git log --author='$(git config --get user.name)' --graph --pretty=format:'%C(cyan)%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(magenta)<%an>%Creset' --abbrev-commit --date=relative"
 
 # remove branches marked as gone
 alias gone="git fetch --all --prune; git branch -vv | awk '/: gone]/{print \$1}' | xargs -r git branch -D"
